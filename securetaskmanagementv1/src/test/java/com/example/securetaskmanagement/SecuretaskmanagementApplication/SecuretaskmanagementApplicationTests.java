4adb7a569bc1e4a67d6847df4ee361a934c6e02f
cod_mac <cod_mac@MacBook-Air-2.local>
2026-02-01 18:37:15 +0600

    Add fresh Spring Boot 3.4.10 project

---
+++ securetaskmanagementv1/src/test/java/com/example/securetaskmanagement/SecuretaskmanagementApplication/SecuretaskmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securetaskmanagement.SecuretaskmanagementApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuretaskmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
